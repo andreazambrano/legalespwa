b15097fd884efe71ab9f3679bbea1657caa0bbde
root <root@buckapi>
2022-06-25 16:12:06 -0400

    legales

--- src/app/services/script.store.ts
+++ src/app/services/script.store.ts
@@ -4,12 +4,22 @@
     src: string;
 }  
 export const ScriptStore: Scripts[] = [
-    {name: 'modernizr', src: './assets/assets/js/vendor/modernizr-3.11.7.min.js'},
-    {name: 'jquery', src: './assets/assets/js/vendor/jquery-3.6.0.min.js'},
-    {name: 'jquery-migrate', src: './assets/assets/js/vendor/jquery-migrate-3.3.2.min.js'},
-    {name: 'bootstrap.bundle', src: './assets/assets/js/vendor/bootstrap.bundle.min.js'},
-    {name: 'swiper-bundle', src: './assets/assets/js/plugins/swiper-bundle.min.js'},
-    {name: 'fancybox', src: './assets/assets/js/plugins/fancybox.min.js'},
-    {name: 'jquery.nice-select', src: './assets/assets/js/plugins/fancybox.min.js'},
-    {name: 'main', src: './assets/assets/js/main.js'}
+    {name: 'jquery', src: './assets/assetsL/js/jquery.js'},
+   {name: 'modernizr', src: './assets/assets/js/vendor/modernizr-3.11.7.min.js'},
+   {name: 'bootstrap', src: './assets/assetsL/js/bootstrap.min.js'},
+   {name: 'jquery', src: './assets/assetsL/js/jquery.mCustomScrollbar.concat.min.js'},
+   {name: 'jquery', src: './assets/assetsL/js/jquery.fancybox.js'},
+   {name: 'appear', src: './assets/assetsL/js/appear.js'},
+   {name: 'nav', src: './assets/assetsL/js/nav-tool.js'},
+   {name: 'mixitup', src: './assets/assetsL/js/mixitup.js'},
+   {name: 'owl', src: './assets/assetsL/js/owl.js'},
+   {name: 'wow', src: './assets/assetsL/js/wow.js'},
+   {name: 'knob', src: './assets/assetsL/js/knob.js'},
+   {name: 'isotope', src: './assets/assetsL/js/isotope.js'},
+   {name: 'jquery', src: './assets/assetsL/js/jquery-ui.js'},
+   {name: 'script', src: './assets/assetsL/js/script.js'},
+   {name: 'color', src: './assets/assetsL/js/color-settings.js'}
+
 ];
+
+
